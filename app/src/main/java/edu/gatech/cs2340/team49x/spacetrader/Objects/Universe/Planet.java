0920068dbfbf09023aeb908f56f0f0c45ca656c2
Kalen Patton <kpatton33@gatech.edu>
2019-04-05 16:24:39 -0400

    More code check fixes

--- app/src/main/java/edu/gatech/cs2340/team49x/spacetrader/Objects/Universe/Planet.java
+++ app/src/main/java/edu/gatech/cs2340/team49x/spacetrader/Objects/Universe/Planet.java
@@ -2,7 +2,7 @@ package edu.gatech.cs2340.team49x.spacetrader.Objects.Universe;
 
 import java.io.Serializable;
 
-public class Planet implements Serializable {
+class Planet implements Serializable {
 
     static final long serialVersionUID = 1L;
 
